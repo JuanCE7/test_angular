82a189bd21dd40eefd0df6f3d909bc668ee81021
JuanCE7 <juancastiestrella@gmail.com>
2024-12-16 19:54:53 -0500

    feat: user component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'test_angular';
+  title = 'test de test';
 }
