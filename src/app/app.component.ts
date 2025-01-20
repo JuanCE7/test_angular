7e20e4be012baa38b841954830bfded1df5c3010
JuanCE7 <juancastiestrella@gmail.com>
2025-01-19 21:59:28 -0500

    update: navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,38 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { PrimeNG } from 'primeng/config';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, RouterLink],
+  imports: [RouterOutlet, FormsModule, RouterLink, MenubarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   constructor(private primeng: PrimeNG) {}
 
+  items: MenuItem[] | undefined;
   ngOnInit() {
+    this.items = [
+      {
+        label: 'Inicio',
+        icon: 'pi pi-home',
+        routerLink: '/house',
+      },
+      {
+        label: 'Comentarios',
+        icon: 'pi pi-comments',
+        routerLink: '/comments',
+      },
+      {
+        label: 'Usuarios',
+        icon: 'pi pi-users',
+        routerLink: '/user',
+      },
+    ];
+
     this.primeng.zIndex = {
       modal: 1100, // dialog, sidebar
       overlay: 1000, // dropdown, overlaypanel
